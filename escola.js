953460c766cefd51d213986d3e9fe83928557e7a
GABRIEL RENATO NASCIMENTO MANZOLLA <25203680@CFP127ALUNO.LOCAL>
2026-02-13 16:56:03 -0300

    first commit

---
+++ escola.js
@@ -0,0 +1,54 @@
+/*************************************************************************************
+ *  Objetivo: projeto para realizar calculos para uma escola
+ *  Autor: gabriel renato
+ *  Data: 29/01/26
+ *  Versão: 1.0
+ * ***********************************************************************************/
+
+
+const readline = require('readline')
+
+const entradaDedados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+entradaDedados.question('Qual o primeiro número? ', function(primeiro){
+    let primeiroNumero = primeiro
+
+    entradaDedados.question('Qual o segundo número? ', function(segundo){
+        let segundoNumero = segundo
+
+        entradaDedados.question('Qual operação matemática deseja realiza? ', function(math){
+            let operacaomatematica = math
+
+
+    
+        let resultado = require('./modulo.js')
+        let mate = resultado.operacoes(primeiroNumero, segundoNumero, operacaomatematica)
+        
+        if(mate){
+        let calculo = mate
+        //console.log(`o resultado é: ${calculo}`)
+        }
+        
+    
+        
+          
+        
+        })
+    })
+})
+
+
+
+     
+
+    
+
+
+
+
+
+    
---
+++ modulo.js
@@ -0,0 +1,60 @@
+/*************************************************************************************
+ *  Objetivo: projeto para realizar calculos comuns
+ *  Autor: gabriel renato
+ *  Data: 29/01/26
+ *  Versão: 1.0
+ * ***********************************************************************************/
+
+
+
+function operacoes (primeiro, segundo, math){
+    let calculos = math
+    let um = Number(primeiro)
+    let dois = Number(segundo)
+
+    if(um == '' ||isNaN(um) ||isNaN(dois) || dois == ''){
+        console.log('ERRO: Não é permitido letras ou deixar vazio o espaço dos números')
+        return console
+       
+
+    }
+    else if(calculos == 'soma'){
+        let soma = um + dois
+        console.log(soma)
+        return console
+        
+    }
+
+    else if (calculos == 'subtração' || calculos == 'subtracao'){
+        let subtracao = um - dois
+        console.log(subtracao)
+        return console
+        
+    }
+    
+    else if(calculos == 'divisão' || calculos == 'divisao'){
+        let divisao = um/dois
+        if(um == '0' || dois == '0' || dois == '' || um == ''){
+            let zero = 'ERRO: Não é permitido 0 na divisão de nenhum dos lados'
+            console.log(zero)
+            return console
+        }
+        else {console.log(divisao)
+        return console}
+       
+    }
+    
+    else if(calculos == 'multiplicação' || calculos == 'multiplicacao'){
+        let multi = um*dois
+        console.log(multi)
+        return console
+        
+    }
+
+}
+
+
+module.exports = {
+    
+    operacoes
+}
